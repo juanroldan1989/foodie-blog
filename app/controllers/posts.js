d55849982aca2eaa52298ca74b1a67230680d3fa
Juan Roldan <juanroldan1989@gmail.com>
2015-11-13 23:41:35 +0000

    Posts controller - CreatePost action setup

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  actions: {
+    createPost: function() {
+      var post = this.store.createRecord("post", {
+        title: this.get("title"),
+        body: this.get("body")
+      });
+
+      this.setProperties({
+        title: "",
+        body: ""
+      });
+
+      post.save();
+    }
+  }
 });
