53105bb7ae822a6cfbcf05d275cb805552ea80c9
Juan Roldan <juanroldan1989@gmail.com>
2015-11-15 21:39:33 +0000

    Posts controllers "New" action content setup properly

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,19 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    createPost: function() {
-      var post = this.store.createRecord("post", {
-        title: this.get("title"),
-        body: this.get("body")
-      });
-
-      this.setProperties({
-        title: "",
-        body: ""
-      });
-
-      post.save();
-    }
-  }
 });
---
+++ app/controllers/posts/new.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    createPost: function() {
+      var post = this.store.createRecord("post", {
+        userId: 1, //TODO: replace with current_user's ID
+        title: this.get("title"),
+        body: this.get("body")
+      });
+
+      post.save();
+
+      this.setProperties({
+        title: "",
+        body: ""
+      });
+    }
+  }
+});
