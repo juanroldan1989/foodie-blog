57d7b223742b27aecff1d9002e22faf2de38eaf8
Juan Roldan <juanroldan1989@gmail.com>
2015-11-16 00:25:30 +0000

    Application RESTAdapter setup to interact with API

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,12 +1,12 @@
 import DS from "ember-data";
 
 // For prototyping purposes, later to be changed with RestAdapter
-export default DS.FixtureAdapter.extend();
+// export default DS.FixtureAdapter.extend();
 
-// export default DS.RESTAdapter.extend({
-//   host: 'http://localhost:5000',
-//   headers: {
-//     'API_KEY': 'secret key',
-//     'ANOTHER_HEADER': 'Some header value'
-//   }
-// });
+export default DS.RESTAdapter.extend({
+  host: "http://localhost:5000"
+  // headers: {
+  //   'API_KEY': 'secret key',
+  //   'ANOTHER_HEADER': 'Some header value'
+  // }
+});
