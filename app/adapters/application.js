05441d3e9794142c891b30f8538bcf6205e5f8df
Juan Roldan <juanroldan1989@gmail.com>
2015-11-20 06:22:01 +0000

    API version set to V1

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,6 +6,7 @@ export default DS.FixtureAdapter.extend();
 // For real interaction with API
 // export default DS.RESTAdapter.extend({
 //   host: "http://localhost:5000",
+//   namespace: "v1",
 //   pathForType: function(type) {
 //     var underscored = Ember.String.underscore(type);
 
