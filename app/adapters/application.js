045b1e2c27a2951317489036285645693f88a603
Juan Roldan <juanroldan1989@gmail.com>
2016-03-29 20:02:07 +1300

    App Adapter updated - Setup to interact with API in production mode

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,15 +1,16 @@
 import DS from "ember-data";
 
 // For prototyping purposes
-export default DS.FixtureAdapter.extend();
+// export default DS.FixtureAdapter.extend();
 
 // For real interaction with API
-// export default DS.RESTAdapter.extend({
-//   host: "http://localhost:5000",
-//   namespace: "v1",
-//   pathForType: function(type) {
-//     var underscored = Ember.String.underscore(type);
+export default DS.RESTAdapter.extend({
+  // host: "http://localhost:5000",
+  host: "https://foodie-blog-api.herokuapp.com",
+  namespace: "v1",
+  pathForType: function(type) {
+    var underscored = Ember.String.underscore(type);
 
-//     return Ember.String.pluralize(underscored);
-//   }
-// });
+    return Ember.String.pluralize(underscored);
+  }
+});
